9d513f34935f37bb37660a820fdeded685eafa63
Emerie <ezehdavid960@outlook.com>
2025-03-27 16:38:31 +0100

    Updated the javascript

--- js/index.js
+++ js/index.js
@@ -1,19 +1,28 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".navMenu");
 const burgerBtn = document.querySelector(".burger i");
+const navLinks = document.querySelectorAll(".navMenu li a"); 
 
 burger.addEventListener("click", () => {
-  nav.classList.toggle("open");
+  toggleMenu();
+});
 
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (nav.classList.contains("open")) {
+      toggleMenu();
+    }
+  });
+});
+
+function toggleMenu() {
+  nav.classList.toggle("open");
   burgerBtn.classList.toggle("bi-list");
   burgerBtn.classList.toggle("bi-x");
 
-  if (nav.classList.contains("open")) {
-    document.body.style.overflow = "hidden";
-  } else {
-    document.body.style.overflow = "auto";
-  }
-});
-
+  document.body.style.overflow = nav.classList.contains("open")
+    ? "hidden"
+    : "auto";
+}
 
 AOS.init();
